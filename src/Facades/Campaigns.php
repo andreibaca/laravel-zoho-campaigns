c16262203fd449097427037f65cca18625a66e77
Andy <3dandrew@gmail.com>
2024-06-09 16:00:54 +0300

    add doNotMail parameter to facade docs

--- src/Facades/Campaigns.php
+++ src/Facades/Campaigns.php
@@ -8,7 +8,7 @@ use Illuminate\Support\LazyCollection;
 /**
  * @method static string subscribe(string $email, array $contactInfo = [], string $listName = null)
  * @method static string resubscribe(string $email, array $contactInfo = [], string $listName = null)
- * @method static string unsubscribe(string $email, string $listName = null)
+ * @method static string unsubscribe(string $email, ?string $listName = null, bool $do_not_mail = false)
  * @method static LazyCollection subscribers(string $status = 'active', string $sort = 'asc', int $chunkSize = 500, ?string $listName = null)
  * @method static int subscribersCount(string $status = 'active', ?string $listName = null)
  *
